02eb9f9ad794341c55a9cd76695a1828c8487a85
yumeshino <shinozakiyume88@gmail.com>
2022-06-05 12:14:26 +0900

    76.Laravelルート、ビュー

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Udemy
                 </div>
 
                 <div class="links">
