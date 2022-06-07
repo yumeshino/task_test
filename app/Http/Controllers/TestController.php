1124c75d951ce6e341305fcf61ac89b8dd68153a
yumeshino <shinozakiyume88@gmail.com>
2022-06-07 10:15:58 +0900

    82.Laravel MVCモデルの記述法1

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,4 +7,8 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     //
+    public function index()
+    {
+        return view('tests.test');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('tests/test','TestController@index');
