a6621ac1384826293d65d3947ecb0566ad8db66b
yumeshino <shinozakiyume88@gmail.com>
2022-06-09 23:23:40 +0900

    86.クエリビルダ

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,12 +6,20 @@ use Illuminate\Http\Request;
 
 use App\Model\Test;
 
+use Illuminate\Support\Facades\DB;
+
 class TestController extends Controller
 {
     //
     public function index()
     {
         $values = Test::all();
+        
+        $tests = DB::table('tests')
+        ->select('id')
+        ->get();
+
+        dd($tests);
 
         $collection = collect([1, 2, 3, 4, 5, 6, 7]);
 
