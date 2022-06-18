ef7cd79042f8b53e0991fa1b326b42dd4a12bb9d
yumeshino <shinozakiyume88@gmail.com>
2022-06-18 09:43:36 +0900

    補足

--- myLaravel.php
+++ myLaravel.php
@@ -6,3 +6,54 @@ Routing・・・アクセスの振り分け
 Migration・・DBテーブルとの履歴管理
 
 
+コマンド一覧の表示
+	
+php artisan list
+php artisan
+使えるArtisanコマンドの一覧を表示するもの
+
+
+
+ヘルプの表示
+	
+php artisan help コマンド
+php artisan コマンド -h
+このコマンドはコマンドの使い方を確認するもの
+
+
+
+モデル
+
+php artisan make:model ファイル名
+このコマンドを実行することでモデルのテンプレートが作成される
+ファイル名は「ファイル名.php」となっており、中にテンプレート文が記述されている
+
+
+マイグレーション
+マイグレーションとはデータベースへのテーブル定義を管理するもので、マイグレーション機能によってSQL（データベースを構築する際の言語）についての知識がなくてもデータベースを構築することができる
+流れとしては
+
+マイグレーションファイルの作成
+マイグレーションの実行
+	
+php artisan make:migration ファイル名
+ファイル名で指定した名前のマイグレーションファイルを作成することができる
+	
+php artisan make:migration ファイル名 --create=テーブル名
+新規テーブルの作成
+
+	
+php artisan make:migration ファイル名 table=テーブル名
+既存テーブルの構造を変更
+
+	
+php artisan migrate
+作成したマイグレーションファイルのUPファンクションの部分を実行
+
+php artisan migrate:rollback
+最後にマイグレーションを実行する前の状態に戻
+
+	
+php artisan migrate:refresh
+一度すべてのテーブルを削除し、その後もう一度マイグレーションしていきます。
+
